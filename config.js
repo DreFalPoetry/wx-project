f27ca190831f5545e4111a4c613087f2aec6cb67
RanranLiu <864749024@qq.com>
2019-07-24 17:55:58 +0800

    封装小程序的网络请求数据方法，添加classic movie组件

---
+++ config.js
@@ -0,0 +1,3 @@
+export const config = {
+  api_base_url:'https://easy-mock.com/mock/5d380aa7e484556a2f4ec062/v1'
+};
---
+++ utils/http.js
@@ -0,0 +1,39 @@
+import {config} from '../config.js';
+
+const tips = {
+  1:'抱歉出现错误',
+  1005:'sddd',
+  3000:'aaaaa'
+}
+
+class HTTP{
+  request(params){
+    wx.request({
+      url: config.api_base_url + params.url,
+      data: params.data,
+      method: params.method || 'GET',
+      success: (res)=>{
+        let code = res.statusCode.toString()
+        if(code.startsWith('2')){
+          params.success(res.data)
+        }else{
+          let error_code = res.data.error_code;
+          this._show_error(error_code)
+        }
+      },
+      fail: (err)=>{
+        this._show_error(1)
+      }
+    })
+  }
+
+  _show_error(error_code){
+    wx.showToast({
+      title: tips[error_code] || tips[1],
+      icon: 'none',
+      duration: 2000
+    })
+  }
+}
+
+export {HTTP};
