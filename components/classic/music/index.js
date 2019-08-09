ceb537474a1d78b2a44dbee5a0ab22becf4a84b7
RanranLiu <864749024@qq.com>
2019-08-09 14:47:23 +0800

    设置音乐播放页面样式

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -5,14 +5,16 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {},
+  properties: {
+    src:String
+  },
 
   /**
    * 组件的初始数据
    */
   data: {
-    pauseSrc:'images/player@waitting.png',
-    playSrc:'images/player@playing.png'
+    pauseSrc:'images/player@pause.png',
+    playSrc:'images/player@play.png'
   },
 
   /**
